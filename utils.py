2aa6c6271834b0e7d0fed9b60c631d9bebbae1b0
MoCha_MoChi <569610881@qq.com>
2025-04-18 21:30:57 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
-def qa_agent(openai_api_key, memory, uploaded_file, question, base_url = 'https://api.openai.com/v1'):
+def qa_agent(openai_api_key, memory, uploaded_file, question):
     model = ChatOpenAI(model = 'gpt-3.5-turbo', openai_api_key = openai_api_key)
     file_content = uploaded_file.read()
     temp_file_path = 'temp.pdf'
